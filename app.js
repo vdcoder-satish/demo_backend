13ce813ed641cfb068b5c74a99facd1dae93ec95
satish <satishnengineer@gmail.com>
2025-03-04 09:27:40 +0530

    changes

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express=require('express');
 const app=express();
 
 app.listen(4000,()=>{
-  console.log('server started $ hello how are you')  
+  console.log('server started $ hello how are you hi')  
 })
